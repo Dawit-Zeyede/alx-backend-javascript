601967e0c7581eb9451d13d7bb4c908af3bf6e89
Dawit-Zeyede <devazeyede10@gmail.com>
2024-11-26 13:02:58 +0000

    Console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displayMessage that prints in STDOUT the string argument.
+ * @msg {String} msg
+ */
+const displayMessage = (msg) => {
+	console.log(msg);
+};
+
+module.exports = displayMessage;
